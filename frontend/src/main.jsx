66a10d31316ad4c157d8ec1f870e6dee35751506
Yashwant Sirsate <yashwantsirsate1@gmail.com>
2026-01-30 19:06:03 +0530

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Chat from "./pages/Chat";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Chat />
+  </React.StrictMode>
+);
