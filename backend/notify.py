ba91a262bb37162695b272ea6876ea56477bf6df
Yashwant Sirsate <yashwantsirsate1@gmail.com>
2026-01-31 03:22:45 +0530

    Delete backend directory

--- backend/notify.py
+++
@@ -1,8 +0,0 @@
-def send_notification(authority, message, location, priority):
-    return {
-        "sent_to": authority,
-        "message": message,
-        "location": location,
-        "priority": priority,
-        "status": "NOTIFIED"
-    }
